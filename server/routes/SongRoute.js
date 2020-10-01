8588fad867bc86db3482f4af5e72e6aa1b3f4393
aldino9112 <aldino.dian@gmail.com>
2020-10-01 18:33:39 +0700

    lyricovhApi

--- server/routes/SongRoute.js
+++ server/routes/SongRoute.js
@@ -1,9 +1,27 @@
 const route = require('express').Router();
+const { Router } = require('express');
 const SongController = require('../controllers/SongController');
 const authentication = require('../middlewares/authentication');
+const axios = require('axios')
 
-route.use(authentication);
+// route.use(authentication);
 route.get('/',SongController.findAll);
 
+
+
+route.get('/:artist/:songname', (req, res, next) => {
+    const { artist, songname } = req.params
+    axios({
+        method: 'GET',
+        url: `https://api.lyrics.ovh/v1/${artist}/${songname}`
+    })
+     .then(response => {
+         const lyrics = response.data
+         res.status(200).json({
+             lyrics
+         })
+     })
+     .catch(err => next(err))
+})
 
 module.exports = route;
